fa40e76c0dc8eee3e54524cb7faa392adaa1a6ed
Mir Imran <mir.imran@tothenew.com>
2017-04-20 12:20:46 +0530

    Live examples

--- src/com/im/test/EmailService.groovy
+++ src/com/im/test/EmailService.groovy
@@ -4,4 +4,11 @@ class EmailService {
     void sendCancellationEmail(User user, String content){
                //sends cancellation email
     }
+
+    void sendPasswordResetEmail(User user, String content){
+               //sends password reset email
+    }
+    void sendPasswordResetFailureEmail(User user, String content){
+               //sends password reset email
+    }
 }
--- test/com/im/test/UserSpec.groovy
+++ test/com/im/test/UserSpec.groovy
@@ -1,5 +1,6 @@
 package com.im.test
 
+import spock.lang.IgnoreRest
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -26,28 +27,82 @@ class UserSpec extends Specification {
     }
 
 
+   void "After updating the password of a user we send a reset password email containing the new password"(){
 
-    void "We get an encrypted password from a string "(){
-        given: "A password"
-        String password = "password"
+       given: "A user with an old password"
+       User user = new User(password: 'oldPassword')
 
-        and: "A user"
+       and: "Mocked EmailService"
+       def emailService = Mock(EmailService)
+       user.emailService = emailService
+
+       when: "When updatePassword is called"
+       user.updatePassword("newPassword")
+
+       then:
+       user.password == "newPassword"
+       1 * emailService.sendPasswordResetEmail(user, "newPassword")
+       0 * emailService.sendPasswordResetFailureEmail(user, "newPassword")
+   }
+
+
+    void "Encrypt passsord should return encrypted password after validating the passsword"(){
+        given: "A user"
         User user = new User()
 
-        and:
-        def mockedEncrypterService = Mock(PasswordEncrypterService)
+        and: "Mock PasswordEncrypterService"
+
+        def passwordEncrypterService = Mock(PasswordEncrypterService)
+        user.passwordEncrypterService = passwordEncrypterService
 
 
-        and: "Stub encrypt method"
-        mockedEncrypterService.encrypt("password") >> "def"
-        user.passwordEncrypterService = mockedEncrypterService
 
         when:
-        String encryptedPassword = user.encyryptPassword(password)
+        String encrypted = user.encyryptPassword("password")
 
         then:
-        encryptedPassword=="def"
+        1 * passwordEncrypterService.encrypt("password") >> {args->
+            if(args[0]=="password"){
+                return "1234567"
+            }else{
+                return "abcdefghi"
+            }
+        }
+        encrypted == "1234567"
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @IgnoreRest
+    void "Sorted interested in categoris are returned"(){
+        given:
+        User user = Spy(User)
+        List dummyCategories = ["movies", "social-networking"]
+
+
+
+        when:
+        List<String> interestedInCategories = user.getSortedInterestedInCategories()
+
+        then:
+        1 * user.getInterestedInCategories() >> dummyCategories
+        interestedInCategories == dummyCategories
+    }
+
+
 
 
 
